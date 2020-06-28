db60d5e7113f4c68539c67bcbb03c1521682a8a4
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-06-28 13:17:04 +0300

    mouse events for tooltip

--- app/components/bar-chart.js
+++ app/components/bar-chart.js
@@ -1,11 +1,20 @@
 /* eslint-disable ember/avoid-leaking-state-in-ember-objects */
 /* eslint-disable ember/no-jquery */
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 import { select } from 'd3-selection';
 import { scaleLinear, scaleBand } from 'd3-scale';
 
 export default Component.extend({
   data: [],
+
+  tooltipTarget: computed('hoveredLabel', function () {
+    return select(this.$('svg')[0])
+      .selectAll('rect')
+      .filter(data => data.label === this.hoveredLabel)
+      .node();
+  }),
+
   didInsertElement() {
     let counts = this.data.map(item => item.count);
     let yScale = scaleLinear()
@@ -24,8 +33,12 @@ export default Component.extend({
       .attr('width', `${xScale.bandwidth()}%`)
       .attr('height', item => `${yScale(item.count)}%`)
       .attr('x', item => `${xScale(item.label)}%`)
-      .attr('y', item => `${100 - yScale(item.count)}%`);
-    
-    this.set('tooltipTarget', this.$('rect')[0]);
+      .attr('y', item => `${100 - yScale(item.count)}%`)
+      .on('mouseover', data => {
+        this.set('hoveredLabel', data.label);
+      })
+      .on('mouseout', () => {
+        this.set('hoveredLabel', null);
+      });    
   }
 });
