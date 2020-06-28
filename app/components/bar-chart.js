803ad2496abd022b1f6a6a8a5fdda50dd057813e
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-06-28 11:42:09 +0300

    Set y-scale domain

--- app/components/bar-chart.js
+++ app/components/bar-chart.js
@@ -2,6 +2,7 @@
 /* eslint-disable ember/no-jquery */
 import Component from '@ember/component';
 import { select } from 'd3-selection';
+import { scaleLinear } from 'd3-scale';
 
 export default Component.extend({
   authors: [
@@ -12,6 +13,10 @@ export default Component.extend({
   ],
 
   didInsertElement() {
+    let authorCounts = this.authors.map(author => author.count);
+    let yScale = scaleLinear()
+      .domain([0, Math.max(...authorCounts)]);
+
     let svg = select(this.$('svg')[0]);
     svg.selectAll('rect').data(this.authors)
       .enter()
