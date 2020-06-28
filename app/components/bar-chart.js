52e2b6318fff495ae43dd76a68b5ded18598eb49
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-06-28 11:33:48 +0300

    Data-bound d3 selection

--- app/components/bar-chart.js
+++ app/components/bar-chart.js
@@ -1,12 +1,18 @@
+/* eslint-disable ember/avoid-leaking-state-in-ember-objects */
 /* eslint-disable ember/no-jquery */
 import Component from '@ember/component';
 import { select } from 'd3-selection';
 
 export default Component.extend({
+  authors: [
+    { name: 'Mark Twain', count: 15 },
+    { name: 'Virginia Woolf', count: 45 },
+    { name: 'John Steinbeck', count: 23 },
+    { name: 'Ralph Ellison', count: 27 },
+  ],
+
   didInsertElement() {
     let svg = select(this.$('svg')[0]);
-    svg.append('rect')
-      .attr('width', 20)
-      .attr('height', 100);
+    svg.selectAll('rect').data(this.authors);
   }
 });
